e6a5464490337171de0eb5171503fc2797b4f211
Iago Lopes <iago.martinelli.lopes@usp.br>
2020-05-06 15:30:01 -0300

    utils update

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-
 import torch
 import torch.nn as nn
 import random
@@ -39,3 +38,16 @@ def fake_v(batch_size, num_classes):
     v = torch.zeros(batch_size, num_classes + 1)
     v[:, -1] = 1
     return v
+
+
+def exp_lr_scheduler(optimizer, epoch, init_lr=0.001, lr_decay_epoch=80):
+    """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
+    lr = init_lr * (0.1 ** (epoch // lr_decay_epoch))
+
+    if epoch % lr_decay_epoch == 0:
+        print('LR is set to {}'.format(lr))
+
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+    return optimizer
